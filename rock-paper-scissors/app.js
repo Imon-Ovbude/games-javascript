bcfa213f433ac7cde6780c4789e555d794196e36
Imon-Ovbude <imonhimio@gmail.com>
2022-03-16 16:47:01 -0700

    created functionality to get random computer guess

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -4,10 +4,31 @@ const resultDisplay = document.getElementById('result');
 
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
 
 possibleChoices.forEach(choice => {
   choice.addEventListener('click', e => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+
+    generateComputerChoice();
   });
 });
+
+const generateComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock';
+  }
+
+  if (randomNumber === 2) {
+    computerChoice = 'paper';
+  }
+
+  if (randomNumber === 3) {
+    computerChoice = 'scissors';
+  }
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+};
